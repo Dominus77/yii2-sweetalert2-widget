f70b21f2a90476b4e98927cd2c8771fdeb66bd95
Alexey Schevchenko <ivanovosity@gmail.com>
2018-01-15 00:30:13 +0300

    Update tests

--- tests/functional/TestCase.php
+++ tests/functional/TestCase.php
@@ -9,7 +9,7 @@ use yii\web\View;
  * Class TestCase
  * @package tests
  */
-abstract class TestCase extends \PHPUnit_Framework_TestCase
+abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
     public static $params;
 
