28ecae9bf67a371e566fd2a418ca3774a167957d
Alexey Schevchenko <ivanovosity@gmail.com>
2018-01-25 20:50:58 +0300

    Update tests

--- tests/functional/AlertTest.php
+++ tests/functional/AlertTest.php
@@ -1,11 +1,7 @@
 <?php
-
 namespace tests;
-
 use dominus77\sweetalert2\Alert;
 use yii\helpers\Json;
-use yii\web\JsExpression;
-
 /**
  * Class AlertTest
  * @package tests
@@ -24,7 +20,6 @@ class AlertTest extends TestCase
         ];
         $this->assertJson(Json::encode($alert->options), $alert->getOptions());
     }
-
     /**
      * @throws \Exception
      */
@@ -34,7 +29,6 @@ class AlertTest extends TestCase
         $alert = Alert::widget(['useSessionFlash' => true]);
         $this->assertContains('', $alert);
     }
-
     /**
      * @throws \Exception
      */
@@ -48,39 +42,9 @@ class AlertTest extends TestCase
                 'customClass' => 'animated jello',
             ]
         ]);
-
-        /** @var \yii\web\Session $session */
-        $session = $this->getSession();
-        $flashes = $session->getAllFlashes();
-
-        $this->expectOutputString($flashes['success'][0]['title']);
-        print 'Your title';
-    }
-
-    /**
-     * @throws \Exception
-     */
-    public function testRunFlashCallback()
-    {
-        \Yii::$app->session->setFlash('success', [
-            [
-                'title' => 'Are you sure?',
-                'text' => "You won't be able to revert this!",
-                'type' => Alert::TYPE_WARNING,
-                'showCancelButton' => true,
-                'confirmButtonColor' => '#3085d6',
-                'cancelButtonColor' => '#d33',
-                'confirmButtonText' => 'Yes, delete it!',
-            ],
-            [
-                'callback' => "This callback"
-            ]
-        ]);
-
         $alert = Alert::widget(['useSessionFlash' => true]);
         $this->assertContains('', $alert);
     }
-
     /**
      * @throws \Exception
      */
@@ -95,14 +59,5 @@ class AlertTest extends TestCase
             ],
         ]);
         $this->assertContains('', $alert);
-    }
-
-    /**
-     * @param bool|true $useSessionFlash
-     * @return bool|mixed|\yii\web\Session
-     */
-    private function getSession($useSessionFlash = true)
-    {
-        return $useSessionFlash ? \Yii::$app->session : false;
     }
 }
